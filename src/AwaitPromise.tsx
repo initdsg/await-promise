45d61f03050758975ce8fbf54c87b1bb6f8ba547
raziman <raziman.terra@gmail.com>
2024-08-21 16:48:38 +0800

    properly build the component

--- src/AwaitPromise.tsx
+++ src/AwaitPromise.tsx
@@ -1,28 +1,30 @@
+import React from "react";
+
 export interface AwaitPromiseProps<T> {
-  delayMilliseconds?: number; // Delay in milliseconds
-  fn: Promise<T>;
-  children: (result: T) => React.ReactNode;
-  error?: (err: Error) => React.ReactNode;
+    delayMilliseconds?: number; // Delay in milliseconds
+    fn: Promise<T>;
+    children: (result: T) => React.ReactNode;
+    error?: (err: Error) => React.ReactNode;
 }
 
 export default async function AwaitPromise<T>({
-  delayMilliseconds,
-  fn,
-  children,
-  error,
+    delayMilliseconds,
+    fn,
+    children,
+    error,
 }: AwaitPromiseProps<T>) {
-  const delayPromise = new Promise((resolve) =>
-    setTimeout(resolve, delayMilliseconds || 0)
-  );
+    const delayPromise = new Promise((resolve) =>
+        setTimeout(resolve, delayMilliseconds || 0)
+    );
 
-  try {
-    const [result] = await Promise.all([fn, delayPromise]);
-    return <>{children(result)}</>;
-  } catch (err: any) {
-    // TODO: This can be improved under the following article
-    // https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
+    try {
+        const [result] = await Promise.all([fn, delayPromise]);
+        return <>{children(result)}</>;
+    } catch (err: any) {
+        // TODO: This can be improved under the following article
+        // https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
 
-    if (!error) throw err;
-    return <>{error(err)}</>;
-  }
+        if (!error) throw err;
+        return <>{error(err)}</>;
+    }
 }
